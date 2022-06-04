3fd89397c0416c0098c8bb239b6fe8b81ddc7bf8
Nikolay <forsh_nikolay@mail.ru>
2022-06-04 19:50:09 +0300

    cars and dogs

---
+++ src/main/java/ru/forsh/unittesting/model/Car.java
@@ -0,0 +1,55 @@
+package ru.forsh.unittesting.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ */
+public class Car {
+
+    private String manufacturer;
+    private String number;
+    private int year;
+    private String owner;
+
+    private List<String> owners = new ArrayList<>();
+
+
+    public Car(String manufacturer, String number, int year, String owner) {
+        this.manufacturer = manufacturer;
+        this.number = number;
+        this.year = year;
+        this.owner = owner;
+        owners.add(owner);
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+        owners.add(this.owner);
+    }
+
+    public List<String> getOwners() {
+        return owners;
+    }
+}
---
+++ src/test/java/ru/forsh/unittesting/model/CarTest.java
@@ -0,0 +1,60 @@
+package ru.forsh.unittesting.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CarTest {
+
+    private Car car;
+
+    @BeforeEach
+    void createCar() {
+        car = new Car("Skoda", "a234bc", 2019, "Forsh");
+    }
+
+    @Test
+    void getManufacturer() {
+        assertEquals("Skoda", car.getManufacturer());
+    }
+
+    @Test
+    void getNumber() {
+        assertEquals("a234bc", car.getNumber());
+    }
+
+    @Test
+    void setNumber() {
+        car.setNumber("g202tt");
+        assertEquals("g202tt", car.getNumber());
+    }
+
+    @Test
+    void getYear() {
+        assertEquals(2019, car.getYear());
+    }
+
+    @Test
+    void getOwner() {
+        assertEquals("Forsh", car.getOwner());
+    }
+
+    @Test
+    void setOwner() {
+        car.setOwner("Lera");
+        assertEquals("Lera", car.getOwner());
+    }
+
+    @Test
+    void getOwnerTest() {
+        assertArrayEquals(new String[]{"Forsh"}, car.getOwners().toArray());
+    }
+
+    @Test
+    void getOwnerIfOwnerChanges() {
+        car.setOwner("Lera");
+        assertArrayEquals(new String[]{"Forsh", "Lera"}, car.getOwners().toArray());
+    }
+}
